5eb1bc0bc4d56e0d3a3a451d02b6360d18943099
Latitarg <pabloalejandrodelaiglesia@gmail.com>
2023-09-11 20:42:46 -0300

    Nuevo commit tutorial node

--- NodeJS FreeCodeCamp Español/Carpeta 02 - Modulos/app.js
+++ NodeJS FreeCodeCamp Español/Carpeta 02 - Modulos/app.js
@@ -1,4 +1,4 @@
-// Modulo Console
+// 01 - Modulo Console
 
 // console.log("Hola mundo!");
 
@@ -8,10 +8,35 @@
 
 // console.error(new Error("Otro error mas grave!"));
 
-// Modulo Process
+// 02 - Modulo Process
 
 // console.log(process);
 
 // console.log(process.env);
 
-console.log(process.argv);
+// console.log(process.argv);
+
+// console.log(process.memoryUsage());
+
+// 03 - Modulo OS
+// No esta de forma global por ende hay que importarlo
+
+// const os = require('os');
+
+// console.log(os.type());
+
+// console.log(os.homedir());
+
+// console.log(os.uptime());
+
+// console.log(os.userInfo());
+
+// 04 - Modulo timers
+// Estructura del timeout setTimeout(funcion, retraso, argumentos para la funcion separados por coma);
+
+function mostrarTema(tema) {
+    console.log(`Estoy aprendiendo ${tema}`);
+}
+
+mostrarTema("Javascript");
+setTimeout(mostrarTema, 3000, "Node.js");
